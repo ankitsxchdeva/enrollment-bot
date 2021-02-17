db41bc214c15279c34c2ac457261a568f4f3f57c
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-02-17 12:15:14 -0800

    Adder timer

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import time
 from selenium import webdriver
+from datetime import datetime
 driver = webdriver.Firefox(executable_path='/Users/ankitsachdeva/PycharmProjects/autoEnrollUCSC/geckodriver')
 driver.get('https://google.com')
+
+
+class TimingCheck:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    goal_time = "00:00"
+
+    def update_time(self):
+        now = datetime.now()
+        self.current_time = now.strftime("%H:%M")
+
+    def get_time(self):
+        self.update_time(self)
+        return self.current_time
+
+    def print_time(self):
+        self.update_time(self)
+        print("The current time is ", self.current_time)
+
+
+timingChecker = TimingCheck
+timingChecker.print_time(timingChecker)
+
