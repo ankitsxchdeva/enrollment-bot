1258329ec640356fbd6aaf96f1990751321fd4d4
Ankit Sachdeva <38436931+ankitsxchdeva@users.noreply.github.com>
2021-02-17 01:50:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import time
 from selenium import webdriver
-driver = webdriver.Firefox(executable_path='/Users/ankitsachdeva/PycharmProjects/autoEnrollUCSC/geckodriver')
+driver = webdriver.Firefox(executable_path='/Users/ankitsachdeva/PycharmProjects/autoEnrollUCSC/geckodriver') #this path must point to where geckdriver is installed
 driver.get('https://google.com')
