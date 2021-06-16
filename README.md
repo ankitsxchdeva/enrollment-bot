2327f86e357cbdce7bda66a7faf12111ae003bb5
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-06-15 20:28:25 -0700

    restart from scratch

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-This is a simple bot to enroll into classes at UCSC.
-Please note that there are a couple of restrictions when using this application:
-1. Duo authentication should already be filled out
-2. You should already be logged into your MyUCSC account
-3. Your shopping cart should already be filled with all the classes you want to take
-4. All the classes you want to take should have spots available
-5. Regular registration rules apply (prerequisites should be met, no holds on account)
-6. The Selenium driver is setup to be used with GeckoDriver firefox, you can easily adjust it to be used with chrome
-7. I wrote this program half asleep not wanting to register for classes during my ridiculous enrollment time, it is not garunteed to work in any way
-8. 
+## Setup
 
-Please reach out with any questions and concerns.
+- `$ pip install -r requirements.txt`
+-  Add appropriate driver to folder (gecko or chromedriver)
+- Add username and password to .env_example and rename to .env
+- `python3 main.py`
--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import time
+import os
 from selenium import webdriver
-from datetime import datetime
-driver = webdriver.Firefox(executable_path='/Users/ankitsachdeva/PycharmProjects/autoEnrollUCSC/geckodriver')
-driver.get('https://google.com')
+from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+load_dotenv()
+PATH = './geckodriver'
+
+driver = webdriver.Firefox(executable_path=PATH)
+driver.get("https://my.ucsc.edu/")
+time.sleep(2) # wait for redirect to login page
+
+driver.find_element_by_id("username").send_keys(os.environ.get("user"))
+driver.find_element_by_id("password").send_keys(os.environ.get("pass"))
+driver.find_element_by_xpath('//button[text()="Log in"]').click()
+time.sleep(2) # wait for redirect
+driver.find_element_by_xpath('//button[text()="Send Me a Push"]')
+time.sleep(5) # wait for user to approve 2fa
 
 
-class TimingCheck:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    goal_time = "00:00"
 
-    def update_time(self):
-        now = datetime.now()
-        self.current_time = now.strftime("%H:%M")
-
-    def get_time(self):
-        self.update_time(self)
-        return self.current_time
-
-    def print_time(self):
-        self.update_time(self)
-        print("The current time is ", self.current_time)
-
-
-timingChecker = TimingCheck
-timingChecker.print_time(timingChecker)
 
