7270a8d744c796331e03e47fe88cc2a137829018
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-06-15 21:06:51 -0700

    Cant figure out this selenium error

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ time.sleep(2) # wait for redirect to login page
 driver.find_element_by_id("username").send_keys(os.environ.get("user"))
 driver.find_element_by_id("password").send_keys(os.environ.get("pass"))
 driver.find_element_by_xpath('//button[text()="Log in"]').click()
-time.sleep(2) # wait for redirect
-driver.find_element_by_xpath('//button[text()="Send Me a Push"]')
+time.sleep(4) # wait for redirect
+#driver.find_element_by_xpath('/html/body/div/div/div[1]/div/form/div[2]/div/label/input').click()
+driver.find_element_by_xpath('//fieldset/div[1]/button')
+
 time.sleep(5) # wait for user to approve 2fa
 
-
-
-
+#driver.quit()
