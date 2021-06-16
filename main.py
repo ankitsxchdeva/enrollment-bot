942ed7b61372e5d0866760b503d154475aff01b9
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-06-16 02:33:32 -0700

    clean up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import os
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,10 +12,13 @@ from dotenv import load_dotenv
 load_dotenv()
 PATH = './geckodriver'
 driver = webdriver.Firefox(executable_path=PATH)
-target_time = datetime.datetime(2021, 6, 16, 1, 40, 0) # year, month, day, hour, minute
+target_time = datetime.datetime(2021, 6, 16, 2, 33, 0) # year, month, day, hour, minute
 
 print("Target time is " + target_time.strftime("%Y-%m-%d %H:%M:%S"))
 print("Current time is " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+def click(id_str):
+    driver.find_element_by_id(id_str).click()
 
 # Initial
 driver.get("https://my.ucsc.edu/")
@@ -35,27 +39,27 @@ time.sleep(5)
 
 # Enter non-portal in normal mode
 driver.switch_to.default_content()
-driver.find_element_by_id("shibSubmit").click()
+click("shibSubmit")
 time.sleep(2)
-driver.find_element_by_id("win0divPTNUI_LAND_REC_GROUPLET$5").click()
+click("win0divPTNUI_LAND_REC_GROUPLET$5")
 time.sleep(2)
 
 # Wait until target time to enroll into classes
-while datetime.datetime.now() < target_time:
-    time.sleep(1)
+if(target_time > datetime.datetime.now()):
+    time.sleep((target_time - datetime.datetime.now()).total_seconds())
 print("Enrolling now! Time is " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 # Select the correct term
 driver.get("https://my.ucsc.edu/psc/csprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=CAR&EMPLID=1829475&ENRL_REQUEST_ID=&INSTITUTION=INST&STRM=TERM")
-driver.find_element_by_id("DERIVED_SSS_SCT_SSR_PB_GO").click()
 time.sleep(1)
-driver.find_element_by_id("SSR_DUMMY_RECV1$sels$0$$0").click()
-driver.find_element_by_id("DERIVED_SSS_SCT_SSR_PB_GO").click()
+click("SSR_DUMMY_RECV1$sels$0$$0")
+click("DERIVED_SSS_SCT_SSR_PB_GO")
 time.sleep(1)
-driver.find_element_by_id("DERIVED_REGFRM1_LINK_ADD_ENRL$82$").click()
+click("DERIVED_REGFRM1_LINK_ADD_ENRL$82$")
 time.sleep(1)
-driver.find_element_by_id("DERIVED_REGFRM1_SSR_PB_SUBMIT").click()
+click("DERIVED_REGFRM1_SSR_PB_SUBMIT")
 print("Tried to enroll, please check for any errors.")
 
 time.sleep(10)
 driver.quit()
+
