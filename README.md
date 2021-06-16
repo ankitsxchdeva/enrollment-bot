598688805224950729328654413e2f8ebf113b49
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-06-16 01:36:07 -0700

    Added enrollment time setting

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - `$ pip install -r requirements.txt`
 -  Add appropriate driver to folder (gecko or chromedriver)
 - Add username and password to .env_example and rename to .env
-- set row variable to the correct row (defaults to top row)
+- set target_time to your enrollment time, correct format is provided
 - ensure all of the classes you would like to add have been added to your shopping cart
 - `python3 main.py`
+- sit back and relax while your enrollment is conducted for you within 9 seconds of your enrollment time starting!
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,24 +9,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 
 load_dotenv()
-course = "" # set the course id of the class you wish to enroll into
 PATH = './geckodriver'
 driver = webdriver.Firefox(executable_path=PATH)
+target_time = datetime.datetime(2021, 6, 16, 1, 32, 0) # year, month, day, hour, minute
+
+print("Target time is " + target_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Current time is " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+# Initial
 driver.get("https://my.ucsc.edu/")
-time.sleep(2) # wait for redirect
+time.sleep(2) 
 
 # Login
 driver.find_element_by_id("username").send_keys(os.environ.get("user"))
 driver.find_element_by_id("password").send_keys(os.environ.get("pass"))
 driver.find_element_by_xpath('//button[text()="Log in"]').click()
-time.sleep(2) # wait for redirect
+time.sleep(2)
 
 # Handle Duo 2FA
 driver.switch_to.frame("duo_iframe")
 driver.find_element_by_name("dampen_choice").click()
 driver.find_element_by_xpath("//*[@id='auth_methods']/fieldset/div[1]/button").click()
 print("Waiting for duo 2FA")
-time.sleep(5) # wait for redirect
+time.sleep(5)
 
 # Enter non-portal in normal mode
 driver.switch_to.default_content()
@@ -33,6 +39,11 @@ driver.find_element_by_id("shibSubmit").click()
 time.sleep(2)
 driver.find_element_by_id("win0divPTNUI_LAND_REC_GROUPLET$5").click()
 time.sleep(2)
+
+# Wait until target time to enroll into classes
+while datetime.datetime.now() < target_time:
+    time.sleep(1)
+print("Enrolling now! Time is " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 # Select the correct term
 driver.get("https://my.ucsc.edu/psc/csprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=CAR&EMPLID=1829475&ENRL_REQUEST_ID=&INSTITUTION=INST&STRM=TERM")
@@ -44,6 +55,7 @@ time.sleep(3)
 driver.find_element_by_id("DERIVED_REGFRM1_LINK_ADD_ENRL$82$").click()
 time.sleep(3)
 driver.find_element_by_id("DERIVED_REGFRM1_SSR_PB_SUBMIT").click()
+print("Enrolled, please check for any errors.")
 
-#driver.quit()
-# testing classes 70023 and 70330
+time.sleep(10)
+driver.quit()
